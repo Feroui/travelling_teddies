a96262e0f8f8cc6e803feb6129c3e66ba75ba0cb
Nicolas Crestin <nicolas.crestin@hec.edu>
2017-03-20 16:06:17 +0100

    teddiescontroller usercontroller

--- app/controllers/teddies_controller.rb
+++ app/controllers/teddies_controller.rb
@@ -1,2 +1,31 @@
 class TeddiesController < ApplicationController
+
+  def index
+    @teddies = Teddy.all
+  end
+
+  def show
+    @teddy = Teddy.find(params[:id])
+    @stages = Stage.all
+  end
+
+  def new
+    @teddy = Teddy.new
+  end
+
+  def create
+    @teddy = Teddy.new(user_params)
+    @teddy.user = @user
+    @teddy.save
+  end
+
+  private
+
+  def user_params
+    params.require(:teddy).permit(:name, :description, :valid)
+  end
+
+  def get_current_user
+    @user = current_user
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+    current_user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    current_user.destroy
+    redirect_to root_path
+  end
+
+  def dashboard
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :description, :photo, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
