3577284e7818211d740dc6d6c721738fe685097e
Nicolas Crestin <nicolas.crestin@hec.edu>
2017-03-20 17:49:54 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20170320163440) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.boolean  "admin"
     t.string   "provider"
     t.string   "uid"
     t.string   "facebook_picture_url"
@@ -63,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170320163440) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
+    t.boolean  "admin"
     t.string   "photo"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
