a1191d9d4447e9936eba3426091a767e8630748c
Alix Besnier <alix.besnier@gmail.com>
2017-03-22 16:37:12 +0100

    results from search bar

--- app/controllers/teddies_controller.rb
+++ app/controllers/teddies_controller.rb
@@ -2,8 +2,37 @@ class TeddiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @teddies = policy_scope(Teddy).order(created_at: :desc)
-    @stages = Stage.where.not(latitude: nil, longitude: nil)
+    @teddies_all = policy_scope(Teddy).order(created_at: :desc)
+
+    # @last_stages = []
+    # @teddies_all.each do |teddy|
+    #   @last_stages << teddy.stages.last
+    # end
+
+    # @searched_stages = []
+    # @last_stages.each do |stage|
+    #   if stage.adress == params[:address]
+    #     @searched_stages << stage
+    #   end
+    # end
+
+#################
+    @last_stages = Stage.joins("JOIN (SELECT teddy_id, MAX(created_at) AS created_at FROM stages GROUP BY teddy_id) stages2 ON stages.teddy_id = stages2.teddy_id AND stages.created_at = stages2.created_at")
+
+    if params[:address].present?
+      @last_stages = @last_stages.near(params[:address])
+    end
+
+###############
+
+
+    # @teddies = []
+    # @searched_stages.each do |last_stage|
+    #   @teddies << Teddy.find(last_stage.teddy_id)
+    # end
+
+    @teddies = Teddy.where(id: @last_stages.map(&:teddy_id))
+    @stages = @last_stages.where.not(latitude: nil, longitude: nil)
 
     @hash = Gmaps4rails.build_markers(@stages) do |stage, marker|
       marker.lat stage.latitude
