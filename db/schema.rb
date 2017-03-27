cda14bed64c7d4cffc7ef19ae24758fb4b024ccf
Alix Besnier <alix.besnier@gmail.com>
2017-03-27 16:18:10 +0200

    css is better

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324092813) do
+ActiveRecord::Schema.define(version: 20170327134139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170324092813) do
     t.string   "adress"
     t.string   "backpacker_origin"
     t.string   "backpacker_email"
+    t.string   "crush"
     t.index ["teddy_id"], name: "index_stages_on_teddy_id", using: :btree
   end
 
